9b00f2fea50269be246194af60a04ae7b3c49eb9
stufade <stufade223@gmail.com>
2024-05-29 17:57:43 +0300

    +

---
+++ node/index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const server = http.createServer((_, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello world!\n");
+});
+
+const port = 8080;
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
